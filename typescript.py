3f229345e436e0478e687f59288d2f50c7191ab9
Sean Clark Hess <seanhess@gmail.com>
2013-09-12 13:46:46 -0600

    loading tss, loading errors, command-based

--- typescript.py
+++ typescript.py
@@ -1,4 +1,15 @@
-import sublime, sublime_plugin
+import sublime
+from sublime_plugin import WindowCommand
+from sublime_plugin import TextCommand
+from sublime_plugin import EventListener
+from subprocess import Popen, PIPE
+from queue import Queue
+from threading import Thread
+import os
+from functools import partial
+import json
+
+TSS_PATH =  os.path.join(os.path.dirname(__file__),'bin','tss.js')
 
 # NOTE: we need to load the whole project! Search for every file in the whole project and load it in to the plugin, no?
 # As soon as you load something like that... 
@@ -15,27 +26,168 @@ import sublime, sublime_plugin
 # oh, but it should be per-project, right? 
 # depends on how it works :)
 
+# ok, I know how this works, now I just need to figure out what the . 
 
-service = None
+# typescript service needs to be PER FILE
+
+# OR, I could make a global. yeah.
+
+# do we REALLY want to load it like that?
+# well... I don't know. 
+# I would rather it run reliably and slow
+
+# PROBLEM: should use the typescript version of the local repository, no? (Almost impossible)
+# just use the latest version. (Or publish version branches)
+
+# Also: just have it load regularly, and ask the LOCAL version of the errors, no?
+# so... I keep track of the project errors locally, totally parsed. 
+
+class Error(object):
+    file = None
+    start = None
+    end = None
+    text = None
+    phase = None
+    category = None
+    def __init__(self, dict):
+        self.file = dict['file']
+        self.start = ErrorPosition(dict['start'])
+        self.end = ErrorPosition(dict['end'])
+        self.text = dict['text']
+        self.phase = dict['phase']
+        self.category = dict['category']
+
+class ErrorPosition(object):
+    line = 0
+    character = 0
+    def __init__(self, dict):
+        self.line = dict['line']
+        self.character = dict['character']
 
 class TypescriptService(object):
-    blah = []
+    
+    process = None
+    errors = []
+    thread = None
+    queue = None
+    delegate = None
+    loaded = False
 
-class TypescriptCheckCommand(sublime_plugin.TextCommand):
+    def reset(self):
+        if (self.process): self.process.kill()
+        if (self.queue): self.queue_empty()
+        self.queue = Queue()
+        self.thread = Thread(target=partial(self.queue_run))
+        self.thread.daemon = True
+        self.thread.start()
+
+    # forces the loading of the typescript service at this point
+    # unloads any project already loaded
+    # if you can't find a project, just load every file, no?
+    # if you CAN find a project, then use the active one?
+    # only ever have one process
+    def start(self, file_path):
+        print("START: ", file_path)
+        self.reset()
+        kwargs = {}
+        self.loaded = False
+        self.process = Popen(["/usr/local/bin/node", TSS_PATH, file_path], stdin=PIPE, stdout=PIPE, stderr=PIPE, **kwargs)
+
+    def queue_add(self, method):
+        self.queue.put(method)
+        
+    def checkErrors(self):
+        self.process_write(self.process, 'showErrors')
+        infos = self.process_read(self.process)
+        errors = list(map(lambda e: Error(e), infos))
+        if (self.delegate):
+            self.delegate.on_typescript_errors(errors)
+
+    def queue_run(self):
+        item = self.queue.get() # BLOCKING!
+        print("RUN", item)
+        item()
+        self.queue.task_done()
+        self.queue_run()
+
+    # empties the whole queue
+    def queue_empty(self):
+        if (self.queue.empty()): return
+        self.queue.get(False)
+        self.queue_empty()
+
+    def process_read(self, process):
+        line = process.stdout.readline().decode('UTF-8')
+        print("TSS: ", line)
+        if line.startswith('"loaded'):
+            self.loaded = True
+            if (self.delegate):
+                self.delegate.on_typescript_loaded()
+            return self.process_read(process)
+        else:
+            return json.loads(line)
+
+    def process_write(self, process, command):
+        process.stdin.write(bytes(command+'\n','UTF-8'))
+
+
+
+# loads the file in its own process
+# does some AMAZING stuff
+class TypescriptStartCommand(TextCommand):
+    def run(self, edit):
+        service.start(self.view.file_name())
+        service.queue_add(service.checkErrors)
+        service.delegate = self
+        self.wait_for_load(service)
+
+    def is_enabled(self):
+        return isTypescript(self.view)
+
+    def on_typescript_errors(self, errors):
+        print("ERRORS!", errors)
+        self.display_errors(errors)
+
+    def on_typescript_loaded(self):
+        print("LOADED!")
+
+
+    def display_errors(self,errors):
+        self.view.set_status("typescript", "Typescript [%s ERRORS]" % len(errors))
+
+    def wait_for_load(self, service, i=0, dir=1):
+        if (service.loaded):
+            self.display_errors(service.errors)
+        else:
+            before = i % 8
+            after = (7) - before
+            if not after:
+                dir = -1
+            if not before:
+                dir = 1
+            i += dir
+            self.view.set_status("typescript", 'Typescript Loading [%s=%s]' % (' ' *before, ' '*after))
+            sublime.set_timeout(lambda: self.wait_for_load(service, i, dir), 100)
+
+
+
+
+class TypescriptCheckCommand(TextCommand):
     def is_enabled(self):
         return isTypescript(self.view)
 
     def run(self, edit):
         print("Typescript check", edit)
 
-class TypescriptEventListener(sublime_plugin.EventListener):
+
+
+class TypescriptEventListener(EventListener):
 
     # called whenever a veiw is focused
     def on_activated_async(self,view):
-        if(service == None):
-            init()
         self.loaded = "loaded baby"
         print("on_activated_async", service, view.file_name())
+        # if it is a typescript file, and we aren't loaded, run LOAD synchronously. Just burn through it fast
 
     def on_clone_async(self,view):
         print("on_clone_async")
@@ -81,11 +233,7 @@ class TypescriptEventListener(sublime_plugin.EventListener):
     #         view = sublime.active_window().active_view()
     #         return is_ts(view)        
 
-
-def init():
-    print("TYPESCRIPT init")
-    global service
-    service = TypescriptService()
+service = TypescriptService()
 
 def isTypescript(view=None):
     if view is None:
